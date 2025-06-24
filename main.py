d4eb33bbadc2dd5cd291967ed5fc243aca394845
bushikq <zhewang448@gmail.com>
2025-06-24 15:39:25 +0800

    1.1.0

--- main.py
+++ main.py
@@ -13,9 +13,14 @@ from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.microsoft import EdgeChromiumDriverManager # 导入 Edge DriverManager
 from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
 
 
-@register("astrbot_plugin_gameinfo", "bushikq", "一个获取部分二游角色wiki信息的插件", "1.0.5")
+
+@register("astrbot_plugin_gameinfo", "bushikq", "一个获取部分二游角色wiki信息的插件", "1.1.0")
 class FzInfoPlugin(Star):
     def __init__(self, context: Context, config: AstrBotConfig):
         super().__init__(context)
@@ -41,13 +46,17 @@ class FzInfoPlugin(Star):
             self.logger.setLevel(logging.INFO) # 日志等级 可以根据需要调整
         else:
             self.logger.setLevel(logging.ERROR)
-        self.logger.info("明日方舟wiki插件初始化中...")
+        self.logger.info("二游wiki插件初始化中...")
         self.gamelist = {"fz":{"url":"https://prts.wiki/w","output_dir":os.path.join(self.assets_dir, "fzassets"),},
                          "ys":{"url":"https://homdgcat.wiki/gi","output_dir":os.path.join(self.assets_dir, "ysassets"),},
-                         "sr":{"url":"https://homdgcat.wiki/sr","output_dir":os.path.join(self.assets_dir, "srassets")}}
+                         "sr":{"url":"https://homdgcat.wiki/sr","output_dir":os.path.join(self.assets_dir, "srassets")},
+                         "zzz":{"url":"https://zzz3.hakush.in/character","output_dir":os.path.join(self.assets_dir, "zzzassets")},
+                         "ww":{"url":"https://ww2.hakush.in/character","output_dir":os.path.join(self.assets_dir, "wwassets")},
+                         "issac":{"url":"https://isaac.huijiwiki.com/wiki","output_dir":os.path.join(self.assets_dir, "issacassets")},}
         self._handle_config_schema() # 调用处理配置文件方法
+        self._handle_driver_manager() # 调用浏览器驱动管理方法
 
-    def _handle_config_schema(self) -> None:
+    async def _handle_config_schema(self) -> None:
         """处理配置文件,确保它在正确的位置"""
         schema_content ={
             "enable_log_output": {
@@ -69,8 +78,40 @@ class FzInfoPlugin(Star):
         if not config_path.exists():
             with open(config_path, "w", encoding="utf-8") as f:
                 json.dump(schema_content, f, ensure_ascii=False, indent=4)
+    def _handle_driver_manager(self) -> None:
+        self.driver = None
+        service = None
+        if self.browser_type == "edge": # 添加 Edge 支持
+            options = EdgeOptions()
+            options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 Edg/91.0.864.59")
+        elif self.browser_type == "firefox": # 添加 Firefox 支持
+            options = FirefoxOptions()
+            options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0")
+        else: # 默认为 Chrome
+            options = ChromeOptions()
+            options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
+        options.add_argument('--headless')
+        options.add_argument('--disable-gpu')
+        options.add_argument('--window-size=1920,1080')
+        options.add_argument('--ignore-certificate-errors')
+        options.add_argument('--allow-insecure-localhost')
+        options.add_argument('--log-level=3')
+        if self.browser_type == "edge":
+            service = webdriver.edge.service.Service(EdgeChromiumDriverManager().install())
+            self.driver = webdriver.Edge(service=service, options=options)
+        elif self.browser_type == "firefox":
+            service = webdriver.firefox.service.Service(GeckoDriverManager().install())
+            self.driver = webdriver.Firefox(service=service, options=options)
+        else:
+            service = webdriver.chrome.service.Service(ChromeDriverManager().install())
+            self.driver = webdriver.Chrome(service=service, options=options)
     
     async def game_info_handler(self, event: AstrMessageEvent, game: str = None, character: str = None):
+        if not character:
+            yield event.plain_result("角色名不能为空")
+            return
+        if game not in self.gamelist:
+            yield event.plain_result("未知游戏")
         output_dir = self.gamelist[game]["output_dir"]
         os.makedirs(output_dir, exist_ok=True)
         output_path = os.path.join(output_dir, f"{character}.png")
@@ -82,40 +123,69 @@ class FzInfoPlugin(Star):
                 os.remove(output_path)# 缓存过期，删除旧的截图
         url = await self.get_url(game=game, character=character)
         if not url:
-            yield event.text_result("游戏名输入错误，请重新输入")
-            return
-        if not character:
-            yield event.text_result("角色名不能为空")
+            yield event.plain_result("url获取失败")
             return
         try:
             await self.take_full_screenshot(url, output_path, 2)
             yield event.image_result(output_path)
         except Exception as e:
             self.logger.error(f"截图失败: {str(e)}")
+
     @filter.command("srinfo")
     async def sr_handler(self, event: AstrMessageEvent, character: str = None):
         """输入 srinfo [角色名]    返回角色信息截图""" 
         async for ret in self.game_info_handler(event=event, game="sr", character=character):
             yield ret 
-
     @filter.command("fzinfo")
     async def fz_handler(self, event: AstrMessageEvent, character: str = None):
         """输入 fzinfo [角色名]    返回角色信息截图""" 
         async for ret in self.game_info_handler(event=event, game="fz", character=character):
             yield ret
-
     @filter.command("ysinfo")
     async def ys_handler(self, event: AstrMessageEvent, character: str = None):
         """输入 ysinfo [角色名]    返回角色信息截图""" 
         async for ret in self.game_info_handler(event=event, game="ys", character=character):
             yield ret
+    @filter.command("zzzinfo")
+    async def zzz_handler(self, event: AstrMessageEvent, character: str = None):
+        """输入 zzzinfo [角色名]    返回角色信息截图""" 
+        async for ret in self.game_info_handler(event=event, game="zzz", character=character):
+            yield ret
+    @filter.command("wwinfo")
+    async def ww_handler(self, event: AstrMessageEvent, character: str = None):
+        """输入 wwinfo [角色名]    返回角色信息截图""" 
+        async for ret in self.game_info_handler(event=event, game="ww", character=character):
+            yield ret
+    @filter.command("issacinfo")
+    async def issac_handler(self, event: AstrMessageEvent, character: str = None):
+        """输入 issacinfo [角色名]    返回角色信息截图""" 
+        async for ret in self.game_info_handler(event=event, game="issac", character=character):
+            yield ret
+    
 
     async def get_url(self, game: str, character: str):
         if game in self.gamelist:
+            if game == "zzz" or game == "ww":
+                try:
+                    self.logger.info(f"开始尝试获取url: {character}")
+                    driver = self.driver
+                    driver.get(self.gamelist[game]["url"])
+                    character_link_xpath = f"//a[contains(@href, '/character/') and .//div[contains(text(), '{character.split('/')[0]}')]]"
+                    # 等待角色链接加载并可点击
+                    character_link = WebDriverWait(driver, 10).until(
+                        EC.element_to_be_clickable((By.XPATH, character_link_xpath))
+                    )
+                    # 点击角色链接
+                    url = character_link.get_attribute('href')
+                    self.logger.info(f"获取到url: {url}")
+                    return url
+                except Exception as e:
+                    self.logger.error(f"获取url失败: {str(e)}")
+                    return False
             # if game == "sr":
             #     url = f"{self.gamelist[game]['url']}/{character}/战斗"
-            # else:
-            url = f"{self.gamelist[game]['url']}/{character}"
+            else:
+                url = f"{self.gamelist[game]['url']}/{character}"
             return url
         else:
             return None
@@ -134,61 +204,38 @@ class FzInfoPlugin(Star):
         """
         try:
             self.logger.info(f"开始截图: {url}")
-            driver = None
-            service = None
-            if self.browser_type == "edge": # 添加 Edge 支持
-                options = EdgeOptions()
-            elif self.browser_type == "firefox": # 添加 Firefox 支持
-                options = FirefoxOptions()
-            else: # 默认为 Chrome
-                options = ChromeOptions()
-            options.add_argument('--headless')
-            options.add_argument('--disable-gpu')
-            options.add_argument('--window-size=1920,1080')
-            options.add_argument('--ignore-certificate-errors')
-            options.add_argument('--allow-insecure-localhost')
-            options.add_argument('--log-level=3')
-            if self.browser_type == "edge":
-                service = webdriver.edge.service.Service(EdgeChromiumDriverManager().install())
-                driver = webdriver.Edge(service=service, options=options)
-            elif self.browser_type == "firefox":
-                service = webdriver.firefox.service.Service(GeckoDriverManager().install())
-                driver = webdriver.Firefox(service=service, options=options)
-            else:
-                service = webdriver.chrome.service.Service(ChromeDriverManager().install())
-                driver = webdriver.Chrome(service=service, options=options)
-            with driver:
-                driver.get(url)
-                scroll_segments = 5 # 将页面分成 5 段滚动
-                initial_height = 0
-                scroll_pause_time = .75  # 每段滑动后等待0.75秒，可根据需要调整
+            driver = self.driver
+            driver.get(url)
+            scroll_segments = 5 # 将页面分成 5 段滚动
+            initial_height = 0
+            scroll_pause_time = .75  # 每段滑动后等待0.75秒，可根据需要调整
 
-                for i in range(scroll_segments):
-                    driver.execute_script(f"window.scrollTo(0, document.body.scrollHeight / {scroll_segments} * ({i + 1}));")
-                    time.sleep(scroll_pause_time)
+            for i in range(scroll_segments):
+                driver.execute_script(f"window.scrollTo(0, document.body.scrollHeight / {scroll_segments} * ({i + 1}));")
+                time.sleep(scroll_pause_time)
 
-                    new_height = driver.execute_script("return document.body.scrollHeight")
-                    if new_height > initial_height:
-                        self.logger.info(f"滚动到第 {i+1} 段，新高度: {new_height}px")
-                        initial_height = new_height 
-                    else:
-                        self.logger.info(f"滚动到第 {i+1} 段，高度未变化。")
-                    if i == scroll_segments - 1:
-                         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-                         time.sleep(scroll_pause_time) # 确保最终完全滚动到底部并等待
-                # 最终确认页面内容稳定
-                # WebDriverWait(driver, delay).until(EC.visibility_of_element_located((By.ID, "mw-content-text")))
-                # self.logger.info("页面内容加载完成")
+                new_height = driver.execute_script("return document.body.scrollHeight")
+                if new_height > initial_height:
+                    self.logger.info(f"滚动到第 {i+1} 段，新高度: {new_height}px")
+                    initial_height = new_height 
+                else:
+                    self.logger.info(f"滚动到第 {i+1} 段，高度未变化。")
+                if i == scroll_segments - 1:
+                        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+                        time.sleep(scroll_pause_time) # 确保最终完全滚动到底部并等待
+            # 最终确认页面内容稳定
+            # WebDriverWait(driver, delay).until(EC.visibility_of_element_located((By.ID, "mw-content-text")))
+            # self.logger.info("页面内容加载完成")
 
-                final_total_height = driver.execute_script("return document.body.scrollHeight")
-                self.logger.info(f"页面最终总高度: {final_total_height}px")
-                last_height = final_total_height
-                last_height = 7000 if "ys" in output_path or "sr" in output_path else last_height # 对于不适配的原神崩铁页面进行强制截取长度（待改良）
-                # last_height = driver.execute_script("return document.body.scrollHeight")#测试
-                driver.set_window_size(1920, last_height)
-                driver.save_screenshot(output_path)
-                self.logger.info(f"截图成功保存到: {output_path}")
-                return True
+            final_total_height = driver.execute_script("return document.body.scrollHeight")
+            self.logger.info(f"页面最终总高度: {final_total_height}px")
+            last_height = final_total_height
+            last_height = 7000 if "ys" in output_path or "sr" in output_path else last_height # 对于不适配的原神崩铁页面进行强制截取长度（待改良）
+            # last_height = driver.execute_script("return document.body.scrollHeight")#测试
+            driver.set_window_size(1920, last_height)
+            driver.save_screenshot(output_path)
+            self.logger.info(f"截图成功保存到: {output_path}")
+            return True
                 
         except Exception as e:
             self.logger.error(f"截图失败: {str(e)}", exc_info=True)
@@ -202,4 +249,8 @@ class FzInfoPlugin(Star):
         if success:
             yield event.image_result(output_path)
         else:
-            yield event.text_result("截图失败，请检查URL是否正确")
+            yield event.plain_result("截图失败，请检查URL是否正确")
+    async def terminate(self):
+        """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
+        self.logger.info("退出driver...")
+        self.driver.quit()
